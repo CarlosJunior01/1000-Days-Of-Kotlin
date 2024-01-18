b1e2cdcca8d40ea0fd49b6c554776bdc880b78fe
carlos.junior <carlos.udi@live.com>
2024-01-17 23:07:06 -0300

    [Year_2024][Week_02] Create BasicSyntaxTwo
    Created topics for:
    Control Flow: When

--- app/src/main/java/com/carlosjr/days_of_kotlin/year_2024/january/week_02/BasicSyntaxTwo.kt
+++ app/src/main/java/com/carlosjr/days_of_kotlin/year_2024/january/week_02/BasicSyntaxTwo.kt
@@ -1,6 +1,7 @@
 package com.carlosjr.days_of_kotlin.year_2024.january.week_02
 
 import com.carlosjr.days_of_kotlin.year_2024.january.week_02.BasicSyntaxTwo.Companion.ifInKotlin
+import com.carlosjr.days_of_kotlin.year_2024.january.week_02.BasicSyntaxTwo.Companion.whenInKotlin
 
 class BasicSyntaxTwo {
 
@@ -15,17 +16,48 @@ class BasicSyntaxTwo {
                     "\nIn this example, the if expression evaluates whether a is greater than b. If true, " +
                     "\na is returned; otherwise, b is returned. The result is then stored in the max variable."
 
+        const val whenInKotlin: String =
+            "The when expression in Kotlin is a powerful and versatile alternative to the traditional" +
+                    "\nswitch statement found in other programming languages. It can match a value " +
+                    "\nagainst multiple branches, making it easy to handle various conditions. " +
+                    "\nHere's an example:\n" +
+                "\n" +
+                "fun main() {\n" +
+                "    val number = 3\n" +
+                "\n" +
+                "    when (number) {\n" +
+                "        1 -> println(\"One\")\n" +
+                "        2 -> println(\"Two\")\n" +
+                "        3 -> println(\"Three\")\n" +
+                "        else -> println(\"Unknown number\")\n" +
+                "    }\n" +
+                "}\n" +
+                "In this example, the when expression checks the value of the number variable against " +
+                    "\neach branch. If a match is found, the corresponding block of code is executed."
     }
 }
 
 fun main() {
     val baseSyntax = BasicSyntaxTwo()
 
-    //Control Flow: If, When, and Loops
+    //Control Flow: If
     baseSyntax.printText(ifInKotlin)
     val a = 10
     val b = 20
 
     val max = if (a > b) a else b
     baseSyntax.printText("Maximum value: $max")
+
+    //Control Flow: When
+    baseSyntax.printText(whenInKotlin)
+    val number = 3
+
+    when (number) {
+        1 -> println("One")
+        2 -> println("Two")
+        3 -> println("Three")
+        else -> println("Unknown number")
+    }
+    baseSyntax.printText("Maximum value: $max")
+
 }
