f243f22f2ee0e3c7bf00e49d93a307eff1a876ed
carlos.junior <carlosjunior_southsystem@getnet.com.br>
2024-01-17 22:50:34 -0300

    [Year_2024][Week_02] Create BasicSyntaxTwo
    Created topics for:
    Control Flow: If, When, and Loops.

---
+++ app/src/main/java/com/carlosjr/days_of_kotlin/year_2024/january/week_02/BasicSyntaxTwo.kt
@@ -0,0 +1,31 @@
+package com.carlosjr.days_of_kotlin.year_2024.january.week_02
+
+import com.carlosjr.days_of_kotlin.year_2024.january.week_02.BasicSyntaxTwo.Companion.ifInKotlin
+
+class BasicSyntaxTwo {
+
+    fun <T>printText(text: T) {
+        println("#$text")
+        println("# ---------------------------------------------------------------------------------------- #")
+    }
+    companion object {
+        const val ifInKotlin: String =
+            "In Kotlin, if is not just a statement but an expression, meaning it can return a value. " +
+                    "\nThis allows you to write more concise and expressive code." +
+                    "\nIn this example, the if expression evaluates whether a is greater than b. If true, " +
+                    "\na is returned; otherwise, b is returned. The result is then stored in the max variable."
+
+    }
+}
+
+fun main() {
+    val baseSyntax = BasicSyntaxTwo()
+
+    //Control Flow: If, When, and Loops
+    baseSyntax.printText(ifInKotlin)
+    val a = 10
+    val b = 20
+
+    val max = if (a > b) a else b
+    baseSyntax.printText("Maximum value: $max")
+}
